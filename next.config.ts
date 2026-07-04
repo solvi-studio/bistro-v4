d737e7dbc2f635b851324f7d85d2aaa239d51093
MarcusPhan <marcusphan2512@gmail.com>
2026-07-04 16:09:07 +1000

    wip: cf workers

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  
+  // For CF Workers, we need to include the pg-cloudflare package in the output file tracing includes. This is because the pg-cloudflare package is not included in the default output file tracing includes, and it is required for the application to run correctly on CF Workers.
+  // https://github.com/opennextjs/opennextjs-cloudflare/issues/1214
+  outputFileTracingIncludes: {
+    "**/*": [
+      "./node_modules/pg-cloudflare/dist/**",
+      "./node_modules/pg-cloudflare/esm/**",
+    ],
+  },
 };
 
 export default nextConfig;
